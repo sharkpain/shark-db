a0511c5c347ee6521cb13528236224101f632857
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-03-05 10:30:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -52,18 +52,56 @@ function foc(discordId, message) {
                 discordId: discordId,
                 permissions: {groups: [], permissions: []}
             });
-            newUser.save(err => {
-                if (err) {
-                    apis["core-error"].api.error(err);
-                    message.channel.send("error happen, some features may not work right");
-                    return null;
-                }
-            });
+            if (config.owners.includes(discordId)) {
+                ownerGroup(group => {
+                    if (!group) {
+                        apis["core-error"].api.error(err);
+                        message.channel.send("check ur dms idot, error hit the shitter");
+                        return null;
+                    }
+                    newUser.permissions.groups.push(group);
+                })
+            } else {
+                newUser.save(err => {
+                    if (err) {
+                        apis["core-error"].api.error(err);
+                        message.channel.send("error happen, some features may not work right");
+                        return null;
+                    }
+                });
+            }
         } else {
             getUser(discordId, user => {
                 if(!user) return null;
                 return user;
             })
+        }
+    });
+}
+
+function ownerGroup(cb) {
+    PermGroup.findOne({name: "owner"}, function(err, group) {
+        if(group) {
+            if (err) {
+                apis["core-error"].api.error(err);
+                return cb(null);
+            }
+            cb({name: "owner", _id: group._id});
+        } else {
+            let newGroup = new PermGroup({
+                name: "owner",
+                permissions: {
+                    groups: [],
+                    permissions: [{id: "perm", global: true}]
+                }
+            });
+            newGroup.save(err => {
+                if (err) {
+                    apis["core-error"].api.error(err);
+                    return cb(null);
+                }
+                cb({name: "owner", _id: newGroup._id});
+            });
         }
     });
 }
